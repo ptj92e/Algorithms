1136ca31227fefecdbdaef0ac00c49e5f306608d
Phillip Jones <ptj92e@gmail.com>
2020-12-31 08:14:55 -0600

    Common data structures

---
+++ Python/DataStructures/stack.py
@@ -0,0 +1,19 @@
+# try out the python stack functions
+
+# TODO: create a new empty stack
+
+stack = []
+
+# TODO: push into onto the stak
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+
+# TODO: print the stack contents
+print(stack)
+
+# TODO: pop an item off the stack
+x = stack.pop()
+print(x)
+print(stack)
